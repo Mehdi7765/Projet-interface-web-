02dbb189c5f3abd3644b8cc36003bba3769fe03e
Mehdi7765 <146960390+Mehdi7765@users.noreply.github.com>
2023-10-28 00:44:43 +0200

    Add files via upload

--- page_article.js
+++ page_article.js
@@ -6,3 +6,22 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
+const decrementButtons = document.querySelectorAll('.decrement');
+const incrementButtons = document.querySelectorAll('.increment');
+const countElements = document.querySelectorAll('.count');
+
+for (let i = 0; i < decrementButtons.length; i++) {
+    let count = 0;
+
+    decrementButtons[i].addEventListener('click', function () {
+        if (count > 0) {
+            count--;
+            countElements[i].textContent = count;
+        }
+    });
+
+    incrementButtons[i].addEventListener('click', function () {
+        count++;
+        countElements[i].textContent = count;
+    });
+}
